673d36e80d465c40a188f04606aceade5a44803c
songzhijun <szj2ys@163.com>
2026-02-15 17:07:24 +0800

    arb 0.3.1: remove download_strategy for compatibility

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -1,27 +1,12 @@
-class ArbGitHubReleaseAssetDownloadStrategy < CurlDownloadStrategy
-  def curl_args
-    args = super
-    args << "-H" << "Accept: application/octet-stream"
-
-    token = ENV["HOMEBREW_GITHUB_API_TOKEN"]
-    token ||= ENV["GITHUB_TOKEN"]
-    token ||= ENV["GH_TOKEN"]
-    args << "-H" << "Authorization: Bearer #{token}" if token && !token.empty?
-    args
-  end
-end
-
 class Arb < Formula
   desc "Arb terminal application"
   homepage "https://github.com/szj2ys/arb"
   version "0.3.1"
 
   on_macos do
-    url "https://api.github.com/repos/szj2ys/arb/releases/assets/356191765"
+    url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
     sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
   end
-
-  download_strategy ArbGitHubReleaseAssetDownloadStrategy
 
   def install
     prefix.install Dir["Arb.app"]
