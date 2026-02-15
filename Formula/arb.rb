4c1ac7ef67d5a5e77615fb1d55e9c435844cd84b
songzhijun <szj2ys@163.com>
2026-02-15 16:26:35 +0800

    arb 0.3.1

---
+++ Formula/arb.rb
@@ -0,0 +1,29 @@
+class Arb < Formula
+  desc "Arb terminal application"
+  homepage "https://github.com/szj2ys/arb"
+  version "0.3.1"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
+      sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
+    end
+
+    on_intel do
+      url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
+      sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
+    end
+  end
+
+  def install
+    # The release asset contains Arb.app at top-level.
+    prefix.install Dir["Arb.app"]
+
+    bin.install_symlink prefix/"Arb.app/Contents/MacOS/arb"
+    bin.install_symlink prefix/"Arb.app/Contents/MacOS/arb-gui" => "arb-gui"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/arb --version")
+  end
+end
