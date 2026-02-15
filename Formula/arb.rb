c26f3dfb1f4fcd8a723634864ffaab39a7d41e69
songzhijun <szj2ys@163.com>
2026-02-15 16:35:22 +0800

    arb 0.3.1 (download via api asset)

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -4,15 +4,9 @@ class Arb < Formula
   version "0.3.1"
 
   on_macos do
-    on_arm do
-      url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
-      sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
-    end
-
-    on_intel do
-      url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
-      sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
-    end
+    url "https://api.github.com/repos/szj2ys/arb/releases/assets/356189830"
+    sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
+    header "Accept: application/octet-stream"
   end
 
   def install
