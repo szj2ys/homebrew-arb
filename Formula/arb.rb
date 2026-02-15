acec2ac9f836ebab0036dd59a765f251d1eae91c
songzhijun <szj2ys@163.com>
2026-02-15 16:42:29 +0800

    arb 0.3.1: use github api asset

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -4,8 +4,12 @@ class Arb < Formula
   version "0.3.1"
 
   on_macos do
-    url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
+    url "https://api.github.com/repos/szj2ys/arb/releases/assets/356191765"
     sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
+  end
+
+  def download_strategy
+    GitHubPrivateRepositoryReleaseDownloadStrategy
   end
 
   def install
