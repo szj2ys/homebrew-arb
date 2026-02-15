f93c9cca35aa7c6bb0c3237ace177395a590413a
songzhijun <szj2ys@163.com>
2026-02-15 16:39:07 +0800

    arb 0.3.1: restore github download url

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -4,14 +4,8 @@ class Arb < Formula
   version "0.3.1"
 
   on_macos do
-    url "https://api.github.com/repos/szj2ys/arb/releases/assets/356189830"
+    url "https://github.com/szj2ys/arb/releases/download/v0.3.1/arb_for_update.zip"
     sha256 "8dfcd9113401277f1739a3014e29c777e41400e26e9e9ac5eb4ce56c2f7068fa"
-  end
-
-  # Fetch GitHub release assets via the API so that downloads
-  # work even when the web download URLs return 404.
-  def download_strategy
-    GitHubPrivateRepositoryReleaseDownloadStrategy
   end
 
   def install
